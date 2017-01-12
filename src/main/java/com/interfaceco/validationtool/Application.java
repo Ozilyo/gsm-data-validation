2801fa9c195ed01a19550f7df37c3817c7273df5
Ali Abdalla <ali.abdalla@sabalgroup.com>
2017-01-12 10:21:56 +0300

    feature: importing data from csv file.

--- src/main/java/com/interfaceco/validationtool/Application.java
+++ src/main/java/com/interfaceco/validationtool/Application.java
@@ -1,18 +1,33 @@
 package com.interfaceco.validationtool;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.interfaceco.validationtool.util.storage.StorageProperties;
+import com.interfaceco.validationtool.util.storage.StorageService;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableBatchProcessing
+@EnableConfigurationProperties(StorageProperties.class)
 @ImportResource("classpath:batch/job.xml")
 public class Application {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 }
--- src/main/java/com/interfaceco/validationtool/gsmparams/controller/JobLauncherController.java
+++ src/main/java/com/interfaceco/validationtool/gsmparams/controller/JobLauncherController.java
@@ -1,18 +1,71 @@
 package com.interfaceco.validationtool.gsmparams.controller;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.nio.file.Files;
+import java.util.Enumeration;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.interfaceco.validationtool.util.storage.StorageService;
+
+@Controller
 public class JobLauncherController {
-	
+
 	@Autowired
 	JobLauncher jobLauncher;
-	
+
 	@Autowired
 	Job job;
+
+	private final StorageService storageService;
 	
-	public void handle() throws Exception {
-		
+	private static final String JOB_PARAM = "job";
+
+	@Autowired
+	public JobLauncherController(StorageService storageService) {
+		this.storageService = storageService;
 	}
+
+	@PostMapping("/file-upload")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes, HttpServletRequest request)
+			throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException,
+			JobParametersInvalidException {
+		storageService.store(file);
+		String fileName = file.getOriginalFilename();
+		redirectAttributes.addFlashAttribute("message", "Successfully uploaded " + fileName + "!");
+		
+		JobParametersBuilder builder = extractParameters(request);
+		
+		builder.addString("fileName", fileName);
+		
+		jobLauncher.run(job, builder.toJobParameters());
+		return "redirect:/welcome";
+	}
+
+	private JobParametersBuilder extractParameters(HttpServletRequest request) {
+		JobParametersBuilder builder = new JobParametersBuilder();
+		Enumeration<String> paramNames = request.getParameterNames();
+		while (paramNames.hasMoreElements()) {
+			String paramName = paramNames.nextElement();
+			if (!JOB_PARAM.equals(paramName)) {
+				builder.addString(paramName, request.getParameter(paramName));
+			}
+		}
+		return builder;
+	}
+
 }
